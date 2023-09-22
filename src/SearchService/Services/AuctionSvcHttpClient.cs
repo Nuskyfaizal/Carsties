9346ab738bc057094bafe10c59859f9b3435a927
NuskyF <nusky.faizal@synapsys.lk>
2023-09-22 21:22:13 +0530

    Added a Search Service

---
+++ src/SearchService/Services/AuctionSvcHttpClient.cs
@@ -0,0 +1,26 @@
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Services;
+
+public class AuctionSvcHttpClient
+{
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _config;
+    public AuctionSvcHttpClient(HttpClient httpClient, IConfiguration config)
+    {
+        _config = config;
+        _httpClient = httpClient;
+    }
+
+    public async Task<List<Item>> GetItemsForSearchDb()
+    {
+        var lastUpdated = await DB.Find<Item, string>()
+            .Sort(x => x.Descending(x => x.UpdatedAt))
+            .Project(x => x.UpdatedAt.ToString())
+            .ExecuteFirstAsync();
+
+        return await _httpClient.GetFromJsonAsync<List<Item>>(_config["AuctionServiceUrl"] 
+            + "/api/auctions?date=" + lastUpdated);
+    }
+}
